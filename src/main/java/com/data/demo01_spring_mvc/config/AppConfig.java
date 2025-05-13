b8212dddedb3667fdfadc5b1944f4dcd88e2d73f
Nguyen Van Tuyen <nguyenvantuyen6789@gmail.com>
2025-05-13 16:29:42 +0700

    update

---
+++ src/main/java/com/data/demo01_spring_mvc/config/AppConfig.java
@@ -0,0 +1,25 @@
+package com.data.demo01_spring_mvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.data.demo01_spring_mvc")
+public class AppConfig {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new
+                InternalResourceViewResolver();
+        resolver.setPrefix("/views/");
+        resolver.setSuffix(".jsp");
+
+        return resolver;
+    }
+}
